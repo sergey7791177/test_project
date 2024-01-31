4c57090ec7a96fe1461d956268c74dff4291b6f2
sergey7791177 <sergey7791177@mail.ru>
2024-01-31 21:50:38 +0800

    1

--- src/main/java/my/groupId/Repository.java
+++ src/main/java/my/groupId/Repository.java
@@ -6,12 +6,15 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 @Path("/start")
 public class Repository {
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Path("/getCustomersWithOrder")
+
     public List<Test> books(@QueryParam("size")Integer size, @QueryParam("page")Integer page) {
 
         List <Test> books = new ArrayList<>();
@@ -20,6 +23,14 @@ public class Repository {
         books.add(new Test(1,"Master",300,"Sergey"));
         books.add(new Test(2,"Dyna",150,"Alex"));
         return books;
+
+}
+    public Test getPlacesByQueryParam(
+            @QueryParam("size") int size,
+            @QueryParam("id") int id) {
+        return Test.ok(Book.b(id, size)).build();
+    }
+
 
 
 
